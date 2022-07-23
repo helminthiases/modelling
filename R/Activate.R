33c320bea8a41f303ea10c32835e9f96622da644
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-23 19:49:36 +0100

    Add R/Activate.R

---
+++ R/Activate.R
@@ -0,0 +1,32 @@
+# Title     : Activate.R
+# Objective : Activate
+# Created by: greyhypotheses
+# Created on: 23/07/2022
+
+
+#' Activate Libraries
+#'
+Activate <- function () {
+
+  packages <- c('data.table', 'tidyverse', 'moments', 'rmarkdown', 'latex2exp', 'mapview',
+                'roxygen2', 'healthcareai', 'equatiomatic', 'rstatix', 'matrixStats', 'patchwork',
+                'geoR', 'PrevMap', 'kableExtra', 'bookdown', 'lme4', 'nlme', 'DescTools',
+                'sf', 'raster', 'tmap', 'terra', 'spData', 'tidygeocoder', 'rnaturalearth', 'geodata')
+
+  # Activate
+  .activate <- function (x){
+    library(x, character.only = TRUE)
+    if (x == 'rmarkdown') {library(tinytex)}
+  }
+  lapply(packages[!(packages %in% c('tidyverse', 'healthcareai', 'equatiomatic', 'data.table',
+                                    'terra', 'raster', 'spData'))], .activate)
+
+  # Special Case
+  if ('tidyverse' %in% packages) {
+    lapply(X = c('magrittr', 'dplyr', 'tibble', 'ggplot2', 'stringr', 'lubridate'), .activate)
+  }
+
+  # Active libraries
+  sessionInfo()
+
+}
