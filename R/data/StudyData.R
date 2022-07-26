25cef63b511f24a8c8009981b0512e0a38313a31
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-26 16:04:14 +0100

    Update R/data/StudyData.R

--- R/data/StudyData.R
+++ R/data/StudyData.R
@@ -6,25 +6,28 @@
 
 StudyData <- function(ISO2) {
 
+  selection <- 'hookworm'
+
+  abbreviations <- list('ascariasis' = c('asc_examined', 'asc_positive', 'asc_prevalence'),
+                        'hookworm' = c('hk_examined', 'hk_positive', 'hk_prevalence'),
+                        'trichuriasis' = c('tt_examined', 'tt_positive', 'tt_prevalence'))
+
+  infection <- abbreviations[selection]
+
   path <- file.path(getwd(), 'data', paste0(ISO2, '.csv'))
 
   select <- c('identifier', 'iso2', 'longitude', 'latitude', 'year',
-              'hk_examined', 'hk_positive', 'hk_prevalence',
-              'asc_examined', 'asc_positive', 'asc_prevalence',
-              'tt_examined', 'tt_positive', 'tt_prevalence',
               'improved_sewer', 'unpiped_sewer', 'surface_sewer', 'piped_sewer', 'unimproved_sewer',
               'improved_water', 'unpiped_water', 'surface_water', 'piped_water', 'unimproved_water',
-              'p_density', 'elevation')
+              'p_density', 'elevation', unlist(infection, use.names = FALSE))
 
-  colClasses <- c('identifier' = 'integer', 'iso2' = 'character', 'year' = 'integer',
-                  'hk_examined' = 'integer', 'hk_positive' = 'integer', 'asc_examined' = 'integer', 'asc_positive' = 'integer',
-                  'tt_examined' = 'integer', 'tt_positive' = 'integer')
+  colClasses <- c('identifier' = 'integer', 'iso2' = 'character', 'year' = 'integer')
 
   frame <- data.table::fread(file = path, header = TRUE, select = select, colClasses = colClasses,
                              data.table = FALSE)
   str(frame)
 
-  tidyr::pivot_longer()
+  
 
 
 }
