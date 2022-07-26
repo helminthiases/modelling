de459ea485c28f6728dbdd7d7b1a3792a44836b5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-26 17:59:13 +0100

    Update R/single/StudyData.R

--- R/single/StudyData.R
+++ R/single/StudyData.R
@@ -4,30 +4,39 @@
 # Created on: 23/07/2022
 
 
-StudyData <- function(ISO2) {
+#' Study Data
+#'
+#' @param ISO2: The ISO 3166-1 alpha-2 of a country
+#' @param infection: The infection in focus → ascariasis, hookworm, or trichuriasis
+#'
+StudyData <- function(ISO2, infection) {
 
-  selection <- 'hookworm'
 
+  # The infection of interest
   abbreviations <- list('ascariasis' = c('asc_examined', 'asc_positive', 'asc_prevalence'),
                         'hookworm' = c('hk_examined', 'hk_positive', 'hk_prevalence'),
                         'trichuriasis' = c('tt_examined', 'tt_positive', 'tt_prevalence'))
+  measures <- abbreviations[infection]
 
-  infection <- abbreviations[selection]
 
-  path <- file.path(getwd(), 'data', paste0(ISO2, '.csv'))
-
+  # The fields of interest
   select <- c('identifier', 'iso2', 'longitude', 'latitude', 'year',
               'improved_sewer', 'unpiped_sewer', 'surface_sewer', 'piped_sewer', 'unimproved_sewer',
               'improved_water', 'unpiped_water', 'surface_water', 'piped_water', 'unimproved_water',
-              'p_density', 'elevation', unlist(infection, use.names = FALSE))
-
+              'p_density', 'elevation', unlist(measures, use.names = FALSE))
   colClasses <- c('identifier' = 'integer', 'iso2' = 'character', 'year' = 'integer')
 
+
+  # Reading-in the data
+  path <- file.path(getwd(), 'data', paste0(ISO2, '.csv'))
   frame <- data.table::fread(file = path, header = TRUE, select = select, colClasses = colClasses,
                              data.table = FALSE)
-  str(frame)
 
 
+  # NaN
 
+
+  # Hence
+  return(frame)
 
 }
