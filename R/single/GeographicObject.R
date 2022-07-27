c24485de1aae700ece538ba4173d2da7556c3b91
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-27 23:28:20 +0100

    Add R/single/GeographicObject.R

---
+++ R/single/GeographicObject.R
@@ -0,0 +1,28 @@
+# Title     : GeographicObject.R
+# Objective : Converting from a data frame object, to a geographic object (sf + data.frame)
+# Created by: greyhypotheses
+# Created on: 27/07/2022
+
+#'
+#'
+#' @param data: an object of class data.frame that includes the fields <longitude> & <latitude>
+#'
+GeographicObject <- function (data) {
+
+  # Convert the data frame to a geographic object whose CRS = 4326
+  frame <- sf::st_as_sf(data, coords = c('longitude', 'latitude'), crs = 'EPSG:4326')
+
+  frame$longitude <- as.numeric(st_coordinates(frame)[, 1])
+  frame$latitude <- as.numeric(st_coordinates(frame)[, 2])
+
+
+  # Transforming: from 4326 to 3857
+  EPSG <- 3857 # Web Mercator https://epsg.io/3857
+
+  frame <- sf::st_transform(frame, crs = paste0('EPSG:', EPSG))
+  frame$x <- as.numeric(st_coordinates(frame)[, 1])
+  frame$y <- as.numeric(st_coordinates(frame)[, 2])
+
+  return(frame)
+
+}
