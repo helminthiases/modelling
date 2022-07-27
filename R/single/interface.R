94ff1577ab2eb8327cef3e0cabefa2392b6f4557
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-27 23:28:05 +0100

    Update R/single/interface.R

--- R/single/interface.R
+++ R/single/interface.R
@@ -6,6 +6,7 @@
 
 # functions
 source(file = 'R/single/StudyData.R')
+source(file = 'R/single/GeographicObject.R')
 source(file = 'R/single/DataSplitTemporal.R')
 source(file = 'R/single/InitialDiagnostics.R')
 
@@ -16,20 +17,13 @@ infection <- 'hk'
 frame <- StudyData(ISO2 = ISO2, infection = infection)
 
 
-# geography
-frame <- sf::st_as_sf(frame, coords = c('longitude', 'latitude'), crs = 'EPSG:4326')
-frame$longitude <- as.numeric(st_coordinates(frame)[, 1])
-frame$latitude <- as.numeric(st_coordinates(frame)[, 2])
-
-EPSG <- 3857 # Web Mercator https://epsg.io/3857
-frame <- sf::st_transform(frame, crs = paste0('EPSG:', EPSG))
-frame$x <- as.numeric(st_coordinates(frame)[, 1])
-frame$y <- as.numeric(st_coordinates(frame)[, 2])
+# geographic form
+instances <- GeographicObject(data = frame)
 
 
 # Splitting
 splits <- list(training = 2009, testing = 2015)
-T <- DataSplitTemporal(data = frame, splits = splits)
+T <- DataSplitTemporal(data = instances, splits = splits)
 training <- T$training
 testing <- T$testing
 
