15a785f3f4003c240d187165e106f88c1f3689c9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-28 01:09:13 +0100

    Update R/single/InitialDiagnostics.R

--- R/single/InitialDiagnostics.R
+++ R/single/InitialDiagnostics.R
@@ -10,10 +10,23 @@
 #'
 InitialDiagnostics <- function (data) {
 
-  par(bty = 'n', fg = 'grey')
 
+  # Addressing spat.corr.diagnostic's peculiar identification code rules
+  indices <- data[, 'identifier', drop = FALSE] %>% st_drop_geometry()
+  indices <- indices %>%
+    dplyr::group_by(identifier) %>%
+    summarise(N = n())
+  indices$code <- seq(from = 1, to = base::nrow(indices))
+
+  data <- base::merge(x = data, y = indices, by = 'identifier', all.x = TRUE, sort = FALSE)
+
+
+  # the independent varioables
   terms <- 'improved_sewer + unpiped_sewer + surface_sewer + piped_sewer + unimproved_sewer + log(p_density) + log(elevation)'
 
+
+  # illustrating diagnostics
+  par(bty = 'n', fg = 'grey')
   spat.corr.diagnostic(formula = as.formula(object = paste0('positive ~', terms)),
                        units.m = ~examined,
                        nAGQ = 18,
@@ -21,6 +34,6 @@ InitialDiagnostics <- function (data) {
                        coords = ~I(x / 1000) + I(y / 1000),
                        likelihood = 'Binomial',
                        lse.variogram = TRUE,
-                       ID.coords = as.vector(data$identifier))
+                       ID.coords = as.vector(data$code))
 
 }
