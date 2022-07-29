f5d826552b1d0ba5cf4c66c46ac643a0b952c484
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-29 21:56:24 +0100

    Add R/single/Prior.R

---
+++ R/single/Prior.R
@@ -0,0 +1,31 @@
+# Title     : Prior.R
+# Objective : Investigate probable explanatory variables via generalised
+#             mixed models
+# Created by: greyhypotheses
+# Created on: 29/07/2022
+
+#' Prior
+#'
+#' @description Investigate probable explanatory variables via generalised mixed models
+#'
+#' @param data:
+#' @param expr:
+#' @param limit:
+#'
+Prior <- function (data, expr, limit) {
+
+  source(file = 'R/diagnostics/InitialEstimates.R')
+
+  for (i in seq_len(length.out = limit)) {
+
+    terms <- paste0(expr[i:length(expr)], collapse = ' + ')
+    estimates <- InitialEstimates(data = data, terms = terms)
+    E <- summary(estimates$model)
+
+    cat('\n', terms, '\n AIC: ', E$AICtab['AIC'], '\n')
+    print(E$coefficients)
+    cat('\n\n')
+
+  }
+
+}
