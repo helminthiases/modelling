24ba43629309e14706d998f4c00b9107a6e47c97
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-30 18:41:12 +0100

    Update R/single/BinomialLogisticBayes.R

--- R/single/BinomialLogisticBayes.R
+++ R/single/BinomialLogisticBayes.R
@@ -5,7 +5,16 @@
 
 
 
-BinomialLogisticBayes <- function (training, initial) {
+BinomialLogisticBayes <- function (excerpt, terms) {
+
+
+  source(file = 'R/single/InitialParameterSettings.R')
+
+
+  # Initial parameters, and priors, settings
+  T <- InitialParameterSettings(excerpt = excerpt, terms = terms)
+  initial <- T$initial
+  priors <- T$priors
 
 
   # For PrevMap::control.prior: Initial coefficient estimates
@@ -16,28 +25,19 @@ BinomialLogisticBayes <- function (training, initial) {
   # covariance <- summary(initial$model)$vcov
 
 
-  # For PrevMap::control.prior: The distribution settings of the priors
-  # of the variance/scale parameters
-  natural <- log(initial$settings[c('sigma^2', 'phi', 'tau^2')])
-  deviation <- 1
-  parameters <- list(ln.sigmasqr = c(natural[['sigma^2']], deviation),
-                     ln.phi = c(natural[['phi']], deviation),
-                     ln.tausqr = c(natural[['tau^2']], deviation))
-
-
   # Priors
   # beta.mean = base::rep(x = 0, times = length(coefficients))
   # beta.covar = covariance
   control.prior.settings <- control.prior(beta.mean = coefficients,
                                           beta.covar = diag(base::rep(x = 1, times = length(coefficients))),
-                                          log.normal.sigma2 = parameters[['ln.sigmasqr']],
-                                          log.normal.phi = parameters[['ln.phi']],
-                                          log.normal.nugget = parameters[['ln.tausqr']])
+                                          log.normal.sigma2 = as.vector(priors['log(sigma^2)', ]),
+                                          log.normal.phi = as.vector(priors['phi', ]),
+                                          log.normal.nugget = as.vector(priors['log(tau^2)', ]))
 
 
   # Control settings for the MCMC algorithm used for Bayesian inference
   # base::rep(x = 0, times = length(coefficients))
-  control.mcmc.settings <- control.mcmc.Bayes(n.sim = 5000, burnin = 2000, thin = 1,
+  control.mcmc.settings <- control.mcmc.Bayes(n.sim = 5000, burnin = 2000, thin = 4,
                                               epsilon.S.lim = c(0.01, 0.05), L.S.lim = 2,
                                               start.beta = coefficients,
                                               start.sigma2 = initial$settings['sigma^2'],
@@ -51,7 +51,7 @@ BinomialLogisticBayes <- function (training, initial) {
     formula = positive ~ log(unpiped_sewer) + log(surface_sewer) + log(piped_sewer) + log(p_density) + log(elevation),
     units.m = ~examined,
     coords = ~I(x / 1000) + I(y / 1000),
-    data = training,
+    data = excerpt,
     control.prior = control.prior.settings,
     control.mcmc = control.mcmc.settings,
     kappa = 0.5)
