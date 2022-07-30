556dfb296f9b5ec011d6f3d38fd5ab3f9afc42bf
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-30 21:07:37 +0100

    Update R/functions/InitialGLM.R

--- R/functions/InitialGLM.R
+++ R/functions/InitialGLM.R
@@ -10,16 +10,19 @@
 #'
 #' @param data:
 #' @param expr:
+#' @param variables: A list that identifies the names of the fields
+#'                      list(identifier = ..., tests = ..., positives = ...)
+#'                   in <data>.
 #' @param limit:
 #'
-InitialGLM <- function (data, expr, limit) {
+InitialGLM <- function (data, expr, variables, limit) {
 
   source(file = 'R/diagnostics/InitialEstimates.R')
 
   for (i in seq_len(length.out = limit)) {
 
     terms <- paste0(expr[i:length(expr)], collapse = ' + ')
-    estimates <- InitialEstimates(data = data, terms = terms)
+    estimates <- InitialEstimates(data = data, terms = terms, variables = variables)
     E <- summary(estimates$model)
 
     cat('\n\n')
