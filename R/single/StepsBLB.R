efbb7307da3adb14c8ff4a658e52d822e95be5ec
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-02 02:25:24 +0100

    Update R/single/StepsBLB.R

--- R/single/StepsBLB.R
+++ R/single/StepsBLB.R
@@ -5,7 +5,7 @@
 
 
 
-StepsBLB <- function (data, terms, variables) {
+StepsBLB <- function (training, testing, terms, variables) {
 
   source(file = 'R/single/BinomialLogisticBayes.R')
   source(file = 'R/single/EvaluationMetricsBLB.R')
@@ -21,13 +21,13 @@ StepsBLB <- function (data, terms, variables) {
 
 
   # Model
-  objects <- BinomialLogisticBayes(data = excerpt, terms = terms, variables = variables)
+  objects <- BinomialLogisticBayes(data = training, terms = terms, variables = variables)
   model <- objects$model
 
 
 
   # Valuations (vis-à-vis training points) & Predictions (vis-à-vis testing points)
-  T <- EvaluationMetricsBLB(model = model, excerpt = excerpt, testing = testing)
+  T <- EvaluationMetricsBLB(model = model, training = training, testing = testing)
   valuations <- T$valuations
   predictions <- T$predictions
 
@@ -35,18 +35,18 @@ StepsBLB <- function (data, terms, variables) {
 
   # Is there still evidence of residual spatial correlation?
   # The standardised residuals of the differences/errors/residuals w.r.t. the training points
-  residues <- StandardisedResidual(design = model$D, observed = excerpt$prevalence,
+  residues <- StandardisedResidual(design = model$D, observed = training$prevalence,
                                    estimated = valuations$prevalence$predictions)
 
   # The empirical variogram measures & graph w.r.t. the standardised residual
-  points <- EmpiricalVariogram(data = data.frame(residue = residues, x = excerpt$x, y = excerpt$y))
+  points <- EmpiricalVariogram(data = data.frame(residue = residues, x = training$x, y = training$y))
   spatial <- SpatialGraphs(points = points, limit = 500)
 
 
 
   # Illustrating Accuracy: Diagonals
   estimates <- rbind(
-    data.frame(prevalence = excerpt$prevalence, prediction = valuations$prevalence$predictions, segment = 'training'),
+    data.frame(prevalence = training$prevalence, prediction = valuations$prevalence$predictions, segment = 'training'),
     data.frame(prevalence = testing$prevalence, prediction = predictions$prevalence$predictions, segment = 'testing')
   )
   diagonal <- DiagonalGraphs(estimates = estimates)
