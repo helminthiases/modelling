37c40f0bf795b28d5b71f82f64745c2e0ff3b458
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-02 02:24:51 +0100

    Update R/single/EvaluationMetricsBLM.R

--- R/single/EvaluationMetricsBLM.R
+++ R/single/EvaluationMetricsBLM.R
@@ -8,23 +8,25 @@
 #' Evaluating the Geostatistical Binomial Logistic Model
 #'
 #' @param model: A PrevMap binomial.logistic.MCML model
-#' @param excerpt: The training data
+#' @param training: The training data
 #' @param testing: The testing data
 #'
-EvaluationMetricsBLM <- function (model, excerpt, testing) {
+EvaluationMetricsBLM <- function (model, training, testing) {
 
-  # valuations w.r.t. excerpt points
+  # valuations w.r.t. training points
   valuations <- spatial.pred.binomial.MCML(
     object = model,
-    grid.pred = as.matrix(st_drop_geometry(excerpt[, c('x', 'y')]) / 1000),
+    type = 'joint',
+    grid.pred = as.matrix(st_drop_geometry(training[, c('x', 'y')]) / 1000),
     control.mcmc = control.mcmc.MCML(n.sim = 10000, burnin = 2000, thin = 8),
-    predictors = st_drop_geometry(excerpt),
+    predictors = st_drop_geometry(training),
     scale.predictions = 'prevalence'
   )
 
   # predictions
   predictions <- spatial.pred.binomial.MCML(
     object = model,
+    type = 'joint',
     grid.pred = as.matrix(st_drop_geometry(testing[, c('x', 'y')]) / 1000),
     control.mcmc = control.mcmc.MCML(n.sim = 10000, burnin = 2000, thin = 8),
     predictors = st_drop_geometry(testing),
