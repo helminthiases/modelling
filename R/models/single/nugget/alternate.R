087b8647acfe112fe8fe200b1d16ca26ceeaf615
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-06 02:15:48 +0100

    Update R/models/single/nugget/alternate.R

--- R/models/single/nugget/alternate.R
+++ R/models/single/nugget/alternate.R
@@ -28,13 +28,13 @@ instances <- GeographicObject(data = frame)
 
 
 # splitting; this function
-#   a. spatially splits the data into 2 parts - to avoid ill-conditioned
+#   a. spatially splits the data into <step> parts - to avoid ill-conditioned
 #      problems due to geographically close sites
 #      [1. Advanced Engineering Mathematics by Erwin Kreyszig,
 #       2. https://en.wikipedia.org/wiki/Condition_number]
 #   b. splits one of the parts into training & testing sets such that
 #      they have similar prevalence distributions
-T <- DataSplitFractional(instances = instances, fraction = 0.75)
+T <- DataSplitFractional(instances = instances, fraction = 0.80, step = 3)
 training <- T$training
 testing <- T$testing
 rm(T)
