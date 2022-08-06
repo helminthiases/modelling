b7a89d9be83ed11cd0bf5a655df5d2d3e03b09a3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-06 17:53:37 +0100

    Update R/reference/interface.R

--- R/reference/interface.R
+++ R/reference/interface.R
@@ -9,14 +9,22 @@ source(file = 'R/data/StudyData.R')
 source(file = 'R/reference/Time.R')
 source(file = 'R/reference/Distributions.R')
 source(file = 'R/reference/Sewer.R')
+source(file = 'R/reference/Water.R')
 source(file = 'R/reference/Miscellaneous.R')
+source(file = 'R/reference/Extraneous.R')
 
 
 
-# a data set
+# a data set: TG, MW, etc
 ISO2 <- 'TG'
 infection <- 'hk'
-frame <- StudyData(ISO2 = ISO2, infection = infection, add.extraneous = TRUE)
+add.extraneous <- FALSE
+frame <- StudyData(ISO2 = ISO2, infection = infection, add.extraneous = add.extraneous)
+
+if (ISO2 == 'MW') {
+  frame <- frame[frame$year %in% c(2012, 2017), ]
+}
+
 
 
 # the number of observations per year
@@ -27,10 +35,15 @@ Time(data = frame)
 Distributions(data = frame)
 AggregateSewer(data = frame)
 DisaggregateSewer(data = frame)
+AggregateWater(data = frame)
+DisaggregateWater(data = frame)
 AggregateMiscellaneous(data = frame)
 DisaggregateMiscellaneous(data = frame)
 
-
+if (add.extraneous) {
+  AggregateExtraneous(data = frame)
+  DisaggregateExtraneous(data = frame)
+}
 
 
 
