5bc25bc4c5b881fc0c2bd86641f8c85c0cb50b03
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-10 19:58:21 +0100

    Update R/models/single/free/MetricsBLM.R

--- R/models/single/free/MetricsBLM.R
+++ R/models/single/free/MetricsBLM.R
@@ -5,50 +5,54 @@
 
 
 
-#' Metrics Binomial Logistic MCML
-#'
-#' @param model: A model of class ...
-#' @param training: Training data
-#' @param testing: Testing data
-#' @param initial: Initial settings
-#'
-MetricsBLM <- function (model, training, testing, initial) {
+
+source(file = 'R/models/EvaluationMetrics.R')
+source(file = 'R/models/EvaluationGraphs.R')
+source(file = 'R/models/EvaluationVariogram.R')
+source(file = 'R/models/CoefficientsEstimates.R')
+source(file = 'R/functions/ConfidenceInterval.R')
+source(file = 'R/functions/ErrorMetrics.R')
 
 
-  source(file = 'R/models/EvaluationMetrics.R')
-  source(file = 'R/models/EvaluationGraphs.R')
-  source(file = 'R/functions/StandardisedResidual.R')
-  source(file = 'R/functions/EmpiricalVariogram.R')
+
+# Valuations (vis-à-vis training points) & Predictions (vis-à-vis testing points)
+valuations <- EvaluationMetricsBLM(model = mcml$model, data = training, type = 'marginal')
+predictions <- EvaluationMetricsBLM(model = mcml$model, data = testing, type = 'marginal')
 
 
-  # Valuations (vis-à-vis training points) & Predictions (vis-à-vis testing points)
-  T <- EvaluationMetricsBLM(model = model, training = training, testing = testing, type = 'marginal')
-  valuations <- T$valuations
-  predictions <- T$predictions
+# Illustrating Accuracy: Diagonals
+diagonal <- DoubleDiagonalEvaluationGraphs(
+  training_ = list(prediction = valuations$prevalence$predictions, prevalence = training$prevalence),
+  testing_ = list(prediction = predictions$prevalence$predictions, prevalence = testing$prevalence))
 
 
-  # Is there still evidence of residual spatial correlation?
-  # The standardised residuals of the differences/errors/residuals w.r.t. the training points
-  # Subsequently, the empirical variogram measures & graph w.r.t. the standardised residual
-  residues <- StandardisedResidual(design = model$D, observed = training$prevalence,
-                                   estimated = valuations$prevalence$predictions)
-  points <- EmpiricalVariogram(data = data.frame(residue = residues, x = training$x, y = training$y))
-  spatial <- SpatialEvaluationGraphs(points = points, limit = 500)
+# Is there still evidence of residual spatial correlation?
+# The standardised residuals of the differences/errors/residuals w.r.t. the training points
+# Subsequently, the empirical variogram measures & graph w.r.t. the standardised residual
+T <- EvaluationVariogram(
+  model = mcml$model,
+  data = data.frame(prevalence = training$prevalence, x = training$x, y = training$y,
+                    estimate = valuations$prevalence$predictions))
+T$graph
 
 
-  # Illustrating Accuracy: Diagonals
-  estimates <- rbind(
-    data.frame(prevalence = training$prevalence, prediction = valuations$prevalence$predictions, segment = 'training'),
-    data.frame(prevalence = testing$prevalence, prediction = predictions$prevalence$predictions, segment = 'testing')
-  )
-  diagonal <- DiagonalEvaluationGraphs(estimates = estimates)
+# Coefficients
+variables <- list(strings = c('(Intercept)', 'piped_sewer', 'I(piped_sewer^2)' , 'elevation.km'),
+                  labels = c('(Intercept)', 'piped_sewer', 'I(piped_sewer$^{2}$)' , 'elevation.km'))
+parameters <- c('$\\beta_{0}$', '$\\beta_{1}$', '$\\beta_{2}$', '$\\beta_{3}$')
+coefficients <- CoefficientsEstimatesBLM(model = mcml$model, variables = variables, parameters = parameters)
+coefficients
 
 
-  # Bias, Error, Noise
-  # Upcoming
+# Special
+special <- SpecialConfidenceInterval(estimates = summary(mcml$model))
+special$parameter <- c('$ln(\\sigma^2)$','$ln(\\phi)$')
+row.names(special) <- NULL
+special <- special %>% dplyr::select('parameter', 'estimate', 'lower_ci', 'upper_ci',
+                                     'exp(estimate)', 'exp(lower_ci)', 'exp(upper_ci)')
+special
 
 
-  return(list(model = model, valuations = valuations, predictions = predictions, residues = residues,
-              graph.spatial = spatial, graph.diagonal = diagonal, initial = initial))
-
-}
+# Bias & RMSE
+ErrorMetrics(observed = training$prevalence, estimated = valuations$prevalence$predictions, name = 'training')
+ErrorMetrics(observed = testing$prevalence, estimated = predictions$prevalence$predictions, name = 'testing')
