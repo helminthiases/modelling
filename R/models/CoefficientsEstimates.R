3dadd5291f72518b85d0d3bb849cb5480d12c2e1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-10 18:44:23 +0100

    Update R/models/CoefficientsEstimates.R

--- R/models/CoefficientsEstimates.R
+++ R/models/CoefficientsEstimates.R
@@ -6,11 +6,11 @@
 
 CoefficientsEstimatesBLM <- function (model, variables, parameters) {
 
-  E <- summary(mcml$model)
+  estimates <- summary(model)
 
   reference <- data.frame(variable = variables$strings, coefficient = parameters, row.names = variables$strings)
 
-  coefficients <- E$coefficients %>% data.frame() %>% dplyr::select(!z.value)
+  coefficients <- estimates$coefficients %>% data.frame() %>% dplyr::select(!z.value)
   names(coefficients) <- c('est', 'SE', 'p.value')
   coefficients <- CoefficientConfidenceInterval(parameters = coefficients)
 
@@ -18,6 +18,22 @@ CoefficientsEstimatesBLM <- function (model, variables, parameters) {
   coefficients <- coefficients %>%
     dplyr::select('variable', 'coefficient', 'est', '2.5 %', '97.5 %', 'SE', 'p.value')
   coefficients$variable <- variables$labels
+
+  return(coefficients)
+
+}
+
+
+
+CoefficientsEstimatesBLB <- function (model, variables, parameters) {
+
+  estimates <- summary(model)
+
+  reference <- data.frame(variable = variables$labels, coefficient = parameters, row.names = variables$strings)
+  coefficients <- merge(x = estimates$beta, y = reference, by = 0, all.x = TRUE, sort = FALSE)
+  coefficients <- coefficients %>%
+    dplyr::select('variable', 'coefficient', 'Mean', 'Median', 'Mode', 'StdErr', 'HPD 0.025', 'HPD 0.975')
+  row.names(coefficients) <- NULL
 
   return(coefficients)
 
