496ba82d897c804804deb22dd2b4b395b95f1eb1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-10 05:51:22 +0100

    Add R/functions/ErrorMetrics.R

---
+++ R/functions/ErrorMetrics.R
@@ -0,0 +1,26 @@
+# Title     : ErrorMetrics.R
+# Objective : Error Metrics
+# Created by: greyhypotheses
+# Created on: 10/08/2022
+
+
+#' Error Metrics
+#'
+#' @description Calculates bias & root mean square error
+#'
+#' @param observed
+#' @param estimated
+#' @param name
+#'
+ErrorMetrics <- function (observed, estimated, name) {
+
+  # differences/errors
+  differences <- observed - estimated
+
+  metrics <- data.frame(Bias = round(mean(differences), digits = 3),
+                        RMSE = round(sqrt( sum(differences^2) / length(differences) ), digits = 3),
+                        row.names = name)
+
+  return(metrics)
+
+}
