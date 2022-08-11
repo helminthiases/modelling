66953e76472a5902c42508b3e17816789cb736f2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-11 03:06:17 +0100

    Update R/effects/interface.R

--- R/effects/interface.R
+++ R/effects/interface.R
@@ -40,15 +40,34 @@ baseline <- EffectsBaseline(frame = frame, X = X)
 
 instances <- frame[frame$year == 2015, ]
 row.names(instances) <- NULL
-segment <- EffectsSegment(frame = instances, X = X)
+later <- EffectsSegment(frame = instances, X = X)
 
+instances <- frame[frame$year == 2009, ]
+row.names(instances) <- NULL
+earlier <- EffectsSegment(frame = instances, X = X)
 
 
 # Inspect
 dplyr::bind_rows(baseline$LSE)
-dplyr::bind_rows(segment$LSE)
+dplyr::bind_rows(later$LSE)
+dplyr::bind_rows(earlier$LSE)
 
 baseline_ <- baseline$models
-segment_ <- segment$models
+later_ <- later$models
+earlier_ <- earlier$models
+
 anova(baseline_[[8]], baseline_[[7]], baseline_[[3]], baseline_[[6]], baseline_[[2]])
-anova(segment_[[8]], segment_[[7]], segment_[[3]], segment_[[6]], segment_[[2]])
+anova(later_[[8]], later_[[7]], later_[[3]], later_[[6]], later_[[2]])
+anova(earlier_[[8]], earlier_[[7]], earlier_[[3]], earlier_[[6]], earlier_[[2]])
+
+
+# In focus: 3, 7
+for (i in c(3, 7)) {
+  summary(later_[[i]])
+  summary(earlier_[[i]])
+}
+
+for (i in c(2, 6, 8)) {
+  print(summary(later_[[i]]))
+  print(summary(earlier_[[i]]))
+}
