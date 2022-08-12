66e04d4884178147e840fd6068d805bee799e838
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-12 14:13:30 +0100

    Update R/effects/EffectsBaseline.R

--- R/effects/EffectsBaseline.R
+++ R/effects/EffectsBaseline.R
@@ -9,8 +9,11 @@
 #'
 #' @param frame: The data of a year
 #' @param expressions: A list of formulae expressions
+#' @param variables: A list that identifies the names of the fields
+#'                      list(identifier = ..., tests = ..., positives = ...)
+#'                   in <data>.
 #'
-EffectsBaseline <- function (frame, expressions) {
+EffectsBaseline <- function (frame, expressions, variables) {
 
 
   source(file = 'R/effects/Estimates.R')
@@ -19,7 +22,7 @@ EffectsBaseline <- function (frame, expressions) {
   # Generalised linear mixed models
   .glmm <- function (terms) {
     string <- paste(terms, ' + (1|identifier) + (1|year)', collapse = NULL)
-    model <- Estimates(data = frame, expression = string)
+    model <- Estimates(data = frame, expression = string, variables = variables)
     return(model)
   }
 
