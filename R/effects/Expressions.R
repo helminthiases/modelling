b1acbd4818b95e3a1f582bf8b8fdd2abac1fbf39
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-15 20:04:09 +0100

    Update R/effects/Expressions.R

--- R/effects/Expressions.R
+++ R/effects/Expressions.R
@@ -7,23 +7,34 @@
 Expressions <- function () {
 
   initial <- list('1' = paste('improved_sewer + unimproved_sewer + piped_sewer + log(unpiped_sewer) ',
-                        '+ surface_sewer + log(p_density.k) + elevation.km', collapse = NULL),
-            '2' = 'piped_sewer + log(unpiped_sewer) + surface_sewer + log(p_density.k) + elevation.km',
-            '3' = 'piped_sewer + log(p_density.k) + elevation.km',
-            '4' = 'piped_sewer + I(piped_sewer^2) + surface_sewer + log(p_density.k) + elevation.km',
-            '5' = 'piped_sewer + I(piped_sewer^2) + log(p_density.k) + elevation.km',
-            '6' = 'piped_sewer + surface_sewer + log(p_density.k) + elevation.km',
-            '7' = 'piped_sewer + I(piped_sewer^2) + elevation.km',
-            '8' = 'piped_sewer + surface_sewer + elevation.km',
-            '9' = 'piped_sewer + I(piped_sewer^2) + surface_sewer + elevation.km')
+                        '+ surface_sewer + log(p_density.k) + elevation.km + (1|year)', collapse = NULL),
+            '2' = 'piped_sewer + log(unpiped_sewer) + surface_sewer + log(p_density.k) + elevation.km + (1|year)',
+            '3' = 'piped_sewer + log(p_density.k) + elevation.km + (1|year)',
+            '4' = 'piped_sewer + I(piped_sewer^2) + surface_sewer + log(p_density.k) + elevation.km + (1|year)',
+            '5' = 'piped_sewer + I(piped_sewer^2) + log(p_density.k) + elevation.km + (1|year)',
+            '6' = 'piped_sewer + surface_sewer + log(p_density.k) + elevation.km + (1|year)',
+            '7' = 'piped_sewer + I(piped_sewer^2) + elevation.km + (1|year)',
+            '8' = 'piped_sewer + surface_sewer + elevation.km + (1|year)',
+            '9' = 'piped_sewer + I(piped_sewer^2) + surface_sewer + elevation.km + (1|year)')
+
+  simple <- list('1' = paste('improved_sewer + unimproved_sewer + piped_sewer + log(unpiped_sewer) ',
+                              '+ surface_sewer + log(p_density.k) + elevation.km', collapse = NULL),
+                  '2' = 'piped_sewer + log(unpiped_sewer) + surface_sewer + log(p_density.k) + elevation.km',
+                  '3' = 'piped_sewer + log(p_density.k) + elevation.km',
+                  '4' = 'piped_sewer + I(piped_sewer^2) + surface_sewer + log(p_density.k) + elevation.km',
+                  '5' = 'piped_sewer + I(piped_sewer^2) + log(p_density.k) + elevation.km',
+                  '6' = 'piped_sewer + surface_sewer + log(p_density.k) + elevation.km',
+                  '7' = 'piped_sewer + I(piped_sewer^2) + elevation.km',
+                  '8' = 'piped_sewer + surface_sewer + elevation.km',
+                  '9' = 'piped_sewer + I(piped_sewer^2) + surface_sewer + elevation.km')
 
   excerpt <- list('1' = 'piped_sewer + log(unpiped_sewer) + surface_sewer + log(p_density.k) + elevation.km',
             '2' = 'piped_sewer + log(p_density.k) + elevation.km',
             '3' = 'piped_sewer + surface_sewer + log(p_density.k) + elevation.km',
-            '4' = 'piped_sewer + I(piped_sewer^2) + elevation.km',
+            '4' = 'piped_sewer + I(piped_sewer^2) + elevation.km ',
             '5' = 'piped_sewer + surface_sewer + elevation.km',
             '6' = 'piped_sewer + I(piped_sewer^2) + surface_sewer + elevation.km')
 
-  return(list('1' = initial, '2' = excerpt))
+  return(list('1' = initial, '2' = simple, '3' = excerpt))
 
 }
