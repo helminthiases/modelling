2eb6b65c8169f21ddc0e8c77311052df5a10cbe3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-16 05:00:03 +0100

    Update R/reference/Miscellaneous.R

--- R/reference/Miscellaneous.R
+++ R/reference/Miscellaneous.R
@@ -22,10 +22,11 @@ DisaggregateMiscellaneous <- function (data, pathstr) {
 
   diagram <- ggplot(data = instances, mapping = aes(x = value, y = prevalence, colour = year)) +
     geom_point(alpha = 0.05) +
-    geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ splines::bs(x, df = 3), linetype = 'solid') +
-    geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ x, linetype = 'dashed') +
+    geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ splines::bs(x, df = 3), linetype = 'solid', na.rm = TRUE) +
+    geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ x, linetype = 'dashed', na.rm = TRUE) +
+    scale_y_continuous(breaks = c(0, 0.5, 1.0), limits = c(0, 1.0)) +
     scale_colour_manual(values = c('black', 'orange')) +
-    facet_wrap(~miscellaneous, scales = 'free') +
+    facet_wrap(~miscellaneous, scales = 'free_x') +
     theme_minimal() +
     theme(panel.spacing = unit(x = 3, units = 'lines'),
           panel.grid.minor = element_blank(),
@@ -37,15 +38,16 @@ DisaggregateMiscellaneous <- function (data, pathstr) {
     ylab(label = '\nprevalence\n')
   print(diagram)
   ggsave(filename = file.path(pathstr, 'miscellaneous', 'disaggregateMiscellaneousReal.pdf'),
-         plot = diagram, dpi = 95, height = 250, units = 'px', scale = 1)
+         plot = diagram, dpi = 85, width = 575, height = 250, units = 'px', scale = 1)
 
 
   diagram <- ggplot(data = instances, mapping = aes(x = log(value), y = prevalence, colour = year)) +
     geom_point(alpha = 0.05) +
-    geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ splines::bs(x, df = 3), linetype = 'solid') +
-    geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ x, linetype = 'dashed') +
+    geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ splines::bs(x, df = 3), linetype = 'solid', na.rm = TRUE) +
+    geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ x, linetype = 'dashed', na.rm = TRUE) +
+    scale_y_continuous(breaks = c(0, 0.5, 1.0), limits = c(0, 1.0)) +
     scale_colour_manual(values = c('black', 'orange')) +
-    facet_wrap(~miscellaneous, scales = 'free') +
+    facet_wrap(~miscellaneous, scales = 'free_x') +
     theme_minimal() +
     theme(panel.spacing = unit(x = 3, units = 'lines'),
           panel.grid.minor = element_blank(),
@@ -57,7 +59,7 @@ DisaggregateMiscellaneous <- function (data, pathstr) {
     ylab(label = '\nprevalence\n')
   print(diagram)
   ggsave(filename = file.path(pathstr, 'miscellaneous', 'disaggregateMiscellaneousLN.pdf'),
-         plot = diagram, dpi = 95, height = 250, units = 'px', scale = 1)
+         plot = diagram, dpi = 85, width = 575, height = 250, units = 'px', scale = 1)
 
 }
 
@@ -76,11 +78,12 @@ AggregateMiscellaneous <- function (data) {
     gather(key = 'miscellaneous', value = 'value', -prevalence)
 
   graph <- ggplot(data = instances, mapping = aes(x = value, y = prevalence)) +
-    geom_point(alpha = 0.05) +
+    geom_point(alpha = 0.05, na.rm = TRUE) +
     geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ splines::bs(x, df = 3),
-                linetype = 'solid', colour = 'olivedrab') +
+                linetype = 'solid', colour = 'olivedrab', na.rm = TRUE) +
     geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ x,
-                linetype = 'dashed', colour = 'olivedrab') +
+                linetype = 'dashed', colour = 'olivedrab', na.rm = TRUE) +
+    scale_y_continuous(breaks = c(0, 0.5, 1.0), limits = c(0, 1.0)) +
     facet_wrap(~miscellaneous, scales = 'free') +
     theme_minimal() +
     theme(panel.spacing = unit(x = 3, units = 'lines'),
@@ -94,11 +97,12 @@ AggregateMiscellaneous <- function (data) {
   print(graph)
 
   graph <- ggplot(data = instances, mapping = aes(x = log(value), y = prevalence)) +
-    geom_point(alpha = 0.05) +
+    geom_point(alpha = 0.05, na.rm = TRUE) +
     geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ splines::bs(x, df = 3),
-                linetype = 'solid', colour = 'olivedrab') +
+                linetype = 'solid', colour = 'olivedrab', na.rm = TRUE) +
     geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ x,
-                linetype = 'dashed', colour = 'olivedrab') +
+                linetype = 'dashed', colour = 'olivedrab', na.rm = TRUE) +
+    scale_y_continuous(breaks = c(0, 0.5, 1.0), limits = c(0, 1.0)) +
     facet_wrap(~miscellaneous, scales = 'free') +
     theme_minimal() +
     theme(panel.spacing = unit(x = 3, units = 'lines'),
