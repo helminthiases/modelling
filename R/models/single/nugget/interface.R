4a91917d7c1abf94e93e746482202b01c0928a75
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-16 02:51:58 +0100

    Update R/models/single/nugget/interface.R

--- R/models/single/nugget/interface.R
+++ R/models/single/nugget/interface.R
@@ -12,7 +12,11 @@ source(file = 'R/functions/SpatialSplitting.R')
 source(file = 'R/diagnostics/InitialEstimates.R')
 source(file = 'R/models/single/nugget/BinomialLogisticBayes.R')
 source(file = 'R/models/single/nugget/BinomialLogisticMCML.R')
+source(file = 'R/models/single/nugget/CaseBLM.R')
 
+
+# Case
+option <- 2
 
 
 # A data set
@@ -32,7 +36,7 @@ instances$year <- as.factor(instances$year)
 
 
 # Spatial splitting
-T <- SpatialSplitting(instances = instances, step = 4)
+T <- SpatialSplitting(instances = instances, step = 2)
 training <- T$training
 testing <- T$testing
 rm(T)
@@ -43,18 +47,45 @@ variables <- list(identifier = 'identifier', tests = 'examined', positives = 'po
 
 
 # Diagnostics
-terms <- 'piped_sewer + I(piped_sewer^2) + elevation.km'
+cases <- list(A = 'piped_sewer + I(piped_sewer^2) + elevation.km',
+              B = 'piped_sewer + log(p_density.k) + elevation.km')
+terms <- cases[[option]]
 initial <- InitialEstimates(data = training, terms = terms, variables = variables, kappa = 0.5)
 summary(initial$model)
 initial$settings
 
 
+# Labels, stings, etc
+notes <- list(strings = c('(Intercept)', 'piped_sewer', 'log(p_density.k)' , 'elevation.km'),
+              labels = c('(Intercept)', 'piped_sewer', 'log(p_density.k)' , 'elevation.km'),
+              parameters = c('$\\beta_{0}$', '$\\beta_{1}$', '$\\beta_{2}$', '$\\beta_{3}$'))
+
+
 # Modelling
 
-# ... model, initial
+# ... mcml
 mcml <- BinomialLogisticMCML(data = training, terms = terms, variables = variables, kappa = 0.5)
 
-
-# ... model, initial
+# ... bayes
 bayes <- BinomialLogisticBayes(data = training, terms = terms, variables = variables, kappa = 0.5)
+
+
+
+
+# Evaluating
+
+# ... setting-up
+.directory <- function (pathstr) {
+  if (dir.exists(paths = pathstr)) {
+    base::unlink(pathstr, recursive = TRUE)
+  }
+  if (!dir.exists(paths = pathstr)) {
+    dir.create(path = pathstr, showWarnings = TRUE, recursive = TRUE)
+  }
+}
+
+# ... mcml
+pathstr <- file.path(getwd(), 'warehouse', 'models', 'nugget', 'blm', names(cases)[[option]])
+.directory(pathstr = pathstr)
+CaseBLM(mcml = mcml, training = training, testing = testing, pathstr = pathstr, notes = notes)
 
