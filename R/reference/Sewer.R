38e0f6bcc3700d977d9a522a089ce77963519365
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-22 23:22:06 +0100

    Update R/reference/Sewer.R

--- R/reference/Sewer.R
+++ R/reference/Sewer.R
@@ -74,19 +74,19 @@ DisaggregateSewer <- function (data, pathstr) {
 #'
 AggregateSewer <- function (data) {
 
-  variables <- c('improved_sewer', 'unpiped_sewer', 'surface_sewer', 'piped_sewer', 'unimproved_sewer', 'prevalence')
+  variables <- c('unpiped_sewer', 'surface_sewer', 'piped_sewer', 'prevalence')
 
   instances <- data %>%
     dplyr::select(dplyr::all_of(variables)) %>%
-    gather(key = 'sewage', value = 'access_percentage', -prevalence)
+    gather(key = 'sewage', value = 'access_percentage', -EL)
 
-  graph <- ggplot(data = instances, mapping = aes(x = access_percentage, y = prevalence)) +
+  graph <- ggplot(data = instances, mapping = aes(x = access_percentage, y = EL)) +
     geom_point(alpha = 0.05, na.rm = TRUE) +
     geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ splines::bs(x, df = 3), linetype = 'solid', colour = 'olivedrab') +
     geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ x, linetype = 'dashed', colour = 'olivedrab') +
     scale_x_continuous(breaks = c(0, 0.5, 1.0), limits = c(0, 1.0)) +
     scale_y_continuous(breaks = c(0, 0.5, 1.0), limits = c(0, 1.0)) +
-    facet_wrap(~sewage) +
+    facet_wrap(~sewage, ncol = 2) +
     theme_minimal() +
     theme(panel.spacing = unit(x = 3, units = 'lines'),
           panel.grid.minor = element_blank(),
@@ -94,16 +94,16 @@ AggregateSewer <- function (data) {
           strip.text.x = element_text(face = 'bold', size = 10),
           axis.title.x = element_text(size = 11), axis.title.y = element_text(size = 11),
           axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9)) +
-    xlab(label = '\naccess fraction\n') +
-    ylab(label = '\nprevalence\n')
+    xlab(label = '\naccess fraction\n 1 ≡ 100%\n') +
+    ylab(label = '\nempirical logit (prevalence)\n')
   print(graph)
 
-  graph <- ggplot(data = instances, mapping = aes(x = log(access_percentage), y = prevalence)) +
+  graph <- ggplot(data = instances, mapping = aes(x = log(access_percentage), y = EL)) +
     geom_point(alpha = 0.05, na.rm = TRUE) +
     geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ splines::bs(x, df = 3), linetype = 'solid', colour = 'olivedrab') +
     geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ x, linetype = 'dashed', colour = 'olivedrab') +
     scale_y_continuous(breaks = c(0, 0.5, 1.0), limits = c(0, 1.0)) +
-    facet_wrap(~sewage) +
+    facet_wrap(~sewage, ncol = 2) +
     theme_minimal() +
     theme(panel.spacing = unit(x = 3, units = 'lines'),
           panel.grid.minor = element_blank(),
@@ -111,8 +111,8 @@ AggregateSewer <- function (data) {
           strip.text.x = element_text(face = 'bold', size = 10),
           axis.title.x = element_text(size = 11), axis.title.y = element_text(size = 11),
           axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9)) +
-    xlab(label = '\nln(access fraction)\n') +
-    ylab(label = '\nprevalence\n')
+    xlab(label = '\nln(access fraction)\n ln(1) ≡ ln(100%)\n') +
+    ylab(label = '\nempirical logit (prevalence)\n')
   print(graph)
 
 }
