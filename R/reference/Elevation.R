24fa52c323fd69901ed3ee407205ff11f730dfc1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-23 18:45:39 +0100

    Update R/reference/Elevation.R

--- R/reference/Elevation.R
+++ R/reference/Elevation.R
@@ -22,26 +22,26 @@ DisaggregateElevation <- function (data, pathstr) {
     geom_point(alpha = 0.05) +
     geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ splines::bs(x, df = 3), linetype = 'solid', na.rm = TRUE) +
     geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ x, linetype = 'dashed', na.rm = TRUE) +
-    scale_y_continuous(breaks = c(0, 0.5, 1.0), limits = c(0, 1.0)) +
     scale_colour_manual(values = c('orange', 'black')) +
     theme_minimal() +
     theme(panel.grid.minor = element_blank(),
           panel.grid.major = element_line(size = 0.05),
           strip.text.x = element_text(face = 'bold', size = 10),
           axis.title.x = element_text(size = 11), axis.title.y = element_text(size = 11),
-          axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9)) +
+          axis.text.x = element_text(size = 10), axis.text.y = element_text(size = 10),
+          legend.title = element_text(size = 11), legend.text = element_text(size = 10)) +
     xlab(label = '\nelevation <kilometres>\n') +
-    ylab(label = '\nempirical logit (prevalence)\n')
+    ylab(label = '\nempirical logit (prevalence)\n') +
+    guides(colour = guide_legend(title = 'Year'))
   print(diagram)
   ggsave(filename = file.path(pathstr, 'disaggregateElevationReal.pdf'),
-         plot = diagram, dpi = 85, width = 575, height = 250, units = 'px', scale = 1)
+         plot = diagram, dpi = 85, width = 395, height = 285, units = 'px', scale = 1)
 
 
   diagram <- ggplot(data = instances, mapping = aes(x = log(elevation.km), y = EL, colour = year)) +
     geom_point(alpha = 0.05) +
     geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ splines::bs(x, df = 3), linetype = 'solid', na.rm = TRUE) +
     geom_smooth(se = FALSE, size = 0.25, method = 'lm', formula = y ~ x, linetype = 'dashed', na.rm = TRUE) +
-    scale_y_continuous(breaks = c(0, 0.5, 1.0), limits = c(0, 1.0)) +
     scale_colour_manual(values = c('orange', 'black')) +
     theme_minimal() +
     theme(panel.spacing = unit(x = 3, units = 'lines'),
@@ -49,12 +49,14 @@ DisaggregateElevation <- function (data, pathstr) {
           panel.grid.major = element_line(size = 0.05),
           strip.text.x = element_text(face = 'bold', size = 10),
           axis.title.x = element_text(size = 11), axis.title.y = element_text(size = 11),
-          axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9)) +
+          axis.text.x = element_text(size = 10), axis.text.y = element_text(size = 10),
+          legend.title = element_text(size = 11), legend.text = element_text(size = 10)) +
     xlab(label = '\nln(elevation <kilometres>)\n') +
-    ylab(label = '\nempirical logit (prevalence)\n')
+    ylab(label = '\nempirical logit (prevalence)\n') +
+    guides(colour = guide_legend(title = 'Year'))
   print(diagram)
   ggsave(filename = file.path(pathstr, 'disaggregateElevationLN.pdf'),
-         plot = diagram, dpi = 85, width = 575, height = 250, units = 'px', scale = 1)
+         plot = diagram, dpi = 85, width = 395, height = 285, units = 'px', scale = 1)
 
 }
 
@@ -82,8 +84,8 @@ AggregateElevation <- function (data) {
     theme(panel.grid.minor = element_blank(),
           panel.grid.major = element_line(size = 0.05),
           strip.text.x = element_text(face = 'bold', size = 10),
-          axis.title.x = element_text(size = 12), axis.title.y = element_text(size = 12),
-          axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9)) +
+          axis.title.x = element_text(size = 11), axis.title.y = element_text(size = 11),
+          axis.text.x = element_text(size = 10), axis.text.y = element_text(size = 10)) +
     xlab(label = '\nelevation <kilometres>\n') +
     ylab(label = '\nempirical logit (prevalence)\n')
   print(graph)
@@ -101,7 +103,7 @@ AggregateElevation <- function (data) {
           panel.grid.major = element_line(size = 0.05),
           strip.text.x = element_text(face = 'bold', size = 10),
           axis.title.x = element_text(size = 11), axis.title.y = element_text(size = 11),
-          axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9)) +
+          axis.text.x = element_text(size = 10), axis.text.y = element_text(size = 10)) +
     xlab(label = '\nln(elevation <kilometres>)\n') +
     ylab(label = '\nempirical logit (prevalence)\n')
   print(graph)
