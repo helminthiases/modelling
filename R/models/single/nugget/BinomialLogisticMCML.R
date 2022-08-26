2d60ec1b032954d33c37534fefdd90c0eaf5410f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-26 16:21:20 +0100

    Update R/models/single/nugget/BinomialLogisticMCML.R

--- R/models/single/nugget/BinomialLogisticMCML.R
+++ R/models/single/nugget/BinomialLogisticMCML.R
@@ -32,7 +32,7 @@ BinomialLogisticMCML <- function (data, terms, variables, kappa = 0.5) {
   # Model
   # Note, binomial.logistic.MCML(.) does not evaluate as.formula(.).  Hence, if a spatial.pred.binomial.MCML(.)
   # step is upcoming, use an explicitly written formula.
-  for (i in seq(from = 1, to = 5)) {
+  for (i in seq(from = 1, to = 3)) {
     model <- binomial.logistic.MCML(formula = positive ~ piped_sewer + I(piped_sewer^2) + elevation.km,
                                     units.m = ~examined,
                                     coords = ~I(x / 1000) + I(y / 1000),
