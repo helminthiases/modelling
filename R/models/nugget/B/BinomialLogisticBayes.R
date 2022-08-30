8a7d18115a6f97dd41afba18449b447049165671
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-31 00:10:53 +0100

    Update R/models/nugget/B/BinomialLogisticBayes.R

--- R/models/nugget/B/BinomialLogisticBayes.R
+++ R/models/nugget/B/BinomialLogisticBayes.R
@@ -16,7 +16,7 @@
 BinomialLogisticBayes <- function (data, terms, variables) {
 
 
-  source(file = 'R/models/single/free/InitialParameterSettings.R')
+  source(file = '../../single/free/InitialParameterSettings.R')
 
 
   # Initial parameters, and priors, settings
