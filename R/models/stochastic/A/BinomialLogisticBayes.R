ae7d53ad0a5b4436db755d2e3c0549b2f7e12fef
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-31 02:18:21 +0100

    Update R/models/stochastic/A/BinomialLogisticBayes.R

--- R/models/stochastic/A/BinomialLogisticBayes.R
+++ R/models/stochastic/A/BinomialLogisticBayes.R
@@ -16,7 +16,7 @@
 BinomialLogisticBayes <- function (data, terms, variables) {
 
 
-  source(file = '../../single/stochastic/InitialParameterSettings.R')
+  source(file = 'InitialParameterSettings.R')
 
 
   # Initial parameters, and priors, settings
