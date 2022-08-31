5e66e3cb1b1deb0ce9cbf0bf5d01e68c62f349ca
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-31 02:02:35 +0100

    Update R/models/nugget/A/BinomialLogisticMCML.R

--- R/models/nugget/A/BinomialLogisticMCML.R
+++ R/models/nugget/A/BinomialLogisticMCML.R
@@ -17,7 +17,7 @@
 BinomialLogisticMCML <- function (data, terms, variables, kappa = 0.5) {
 
 
-  source(file = 'InitialParameterSettings.R')
+  source(file = 'R/models/nugget/A/InitialParameterSettings.R')
 
 
   # Initial parameters, and priors, settings; nugget excluded
