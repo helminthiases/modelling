b432beb682d38c8d2e10f565fa51fcf4efb9727a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-31 02:36:59 +0100

    Update R/models/stochastic/A/interface.R

--- R/models/stochastic/A/interface.R
+++ R/models/stochastic/A/interface.R
@@ -5,15 +5,36 @@
 
 
 
-# functions
+# Functions
 source(file = 'R/data/StudyData.R')
 source(file = 'R/functions/GeographicObject.R')
 source(file = 'R/functions/SpatialSplitting.R')
 source(file = 'R/diagnostics/InitialEstimates.R')
-source(file = '../../stochastic/A/BinomialLogisticBayes.R')
+source(file = 'R/models/stochastic/A/BinomialLogisticBayes.R')
+source(file = 'R/models/stochastic/A/CaseBLB.R')
 
 
-# a data set
+
+# Setting-up
+option <- 'A'
+terms <- 'piped_sewer + I(piped_sewer^2) + elevation.km'
+notes <- list(strings = c('(Intercept)', 'piped_sewer', 'I(piped_sewer^2)', 'elevation.km'),
+              labels = c('1', 'piped\\underline{\\hspace{0.125cm}}sewer', 'I(piped\\underline{\\hspace{0.125cm}}sewer$^{2}$)', 'elevation.km'),
+              parameters = c('$\\beta_{0}$', '$\\beta_{1}$', '$\\beta_{2}$', '$\\beta_{3}$'))
+
+
+# ... a function for preparing directories
+.directory <- function (pathstr) {
+  if (dir.exists(paths = pathstr)) {
+    base::unlink(pathstr, recursive = TRUE)
+  }
+  if (!dir.exists(paths = pathstr)) {
+    dir.create(path = pathstr, showWarnings = TRUE, recursive = TRUE)
+  }
+}
+
+
+# The data set
 ISO2 <- 'TG'
 infection <- 'hk'
 frame <- StudyData(ISO2 = ISO2, infection = infection)
@@ -24,11 +45,12 @@ frame <- frame[frame$year == 2015, ]
 row.names(frame) <- NULL
 
 
-# geographic form
+# Geographic form
 instances <- GeographicObject(data = frame)
+instances$year <- as.factor(instances$year)
 
 
-# Spatial Splitting
+# Spatial splitting
 T <- SpatialSplitting(instances = instances, step = 2)
 training <- T$training
 testing <- T$testing
@@ -39,12 +61,16 @@ rm(T)
 variables <- list(identifier = 'identifier', tests = 'examined', positives = 'positive')
 
 
-# Diagnostics
-terms <- 'piped_sewer + I(piped_sewer^2) + elevation.km'
-initial <- InitialEstimates(data = training, terms = terms, variables = variables)
-initial$settings
+# Bayes
+
+# ... diagnostics
+initial <- InitialEstimates(data = training, terms = terms, variables = variables, kappa = 0.5)
 summary(initial$model)
+initial$settings
 
+# ... bayes
+bayes <- BinomialLogisticBayes(data = training, terms = terms, variables = variables, kappa = 0.5)
+pathstr <- file.path(getwd(), 'warehouse', 'models', 'nugget', 'blb', option)
+.directory(pathstr = pathstr)
+CaseBLB(bayes = bayes, training = training, testing = testing, pathstr = pathstr, notes = notes)
 
-# Modelling
-bayes <- BinomialLogisticBayes(data = training, terms = terms, variables = variables)
