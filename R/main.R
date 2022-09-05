c560874619b009d7375a766d58d00a253c74101a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-05 16:26:44 +0100

    Update R/main.R

--- R/main.R
+++ R/main.R
@@ -21,3 +21,19 @@ source(file = 'R/models/nugget/B/interface.R')
 
 # export this project's results to <manuscript>
 source(file = 'R/export.R')
+
+# The <url> list of the modelling results
+list.files(path = file.path(getwd(), 'warehouse'), full.names = TRUE, recursive = TRUE, pattern = '*.*')
+files <- lapply(X = files, FUN = function (x) unlist(base::strsplit(x = x, split = 'modelling'))[2]) %>%
+  unlist()
+URL <- lapply(X = files, FUN = function (x) paste0('https://raw.githubusercontent.com/helminthiases/modelling/master/', x)) %>%
+  unlist()
+
+# ... saving the <url> list of enhanced data files
+utils::write.table(x = data.frame(path = URL), file = file.path(getwd(), 'warehouse', 'data.csv'),
+                   append = FALSE, sep = ',', na = '',
+                   row.names = FALSE, col.names = TRUE, fileEncoding = 'UTF-8')
+
+
+
+
