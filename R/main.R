83bef2002c3f90ebdc8c6e2747ab25faae9cf35e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-05 02:01:16 +0100

    Update R/main.R

--- R/main.R
+++ R/main.R
@@ -4,6 +4,20 @@
 # Created on: 29/07/2022
 
 
+# importing the data files
+source(file = 'R/data/import.R')
+
+# conducting single country exploratory analysis graphs
+source(file = 'R/reference/interface.R')
+
+# preliminary investigation via generalised linear mixed modelling
+source(file = 'R/effects/architectures/interface.R')
+
+# case: piped_sewer + I(piped_sewer^2) + elevation.km
+source(file = 'R/models/nugget/A/interface.R')
+
+# case: piped_sewer + log(p_density.k) + elevation.km
+source(file = 'R/models/nugget/B/interface.R')
 
 # export this project's results to <manuscript>
 source(file = 'R/export.R')
