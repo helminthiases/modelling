92d1b3ac53b2f734b1dfb090b6b2156ffe0ed74a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-05 17:37:33 +0100

    Update R/main.R

--- R/main.R
+++ R/main.R
@@ -4,24 +4,22 @@
 # Created on: 29/07/2022
 
 
+
 # importing the data files
 source(file = 'R/data/import.R')
-
 
 # conducting single country exploratory analysis graphs
 source(file = 'R/reference/interface.R')
 
-
 # preliminary investigation via generalised linear mixed modelling
 source(file = 'R/effects/architectures/interface.R')
-
 
 # case: piped_sewer + I(piped_sewer^2) + elevation.km
 source(file = 'R/models/nugget/A/interface.R')
 
-
 # case: piped_sewer + log(p_density.k) + elevation.km
 source(file = 'R/models/nugget/B/interface.R')
+
 
 
 # The <url> list of the modelling results
@@ -31,7 +29,7 @@ files <- lapply(X = files, FUN = function (x) unlist(base::strsplit(x = x, split
 URL <- lapply(X = files, FUN = function (x) paste0('https://raw.githubusercontent.com/helminthiases/modelling/master/', x)) %>%
   unlist()
 
-# ... saving the <url> list of enhanced data files
+# ... saving the <url> list of modelling results
 utils::write.table(x = data.frame(path = URL), file = file.path(getwd(), 'warehouse', 'data.csv'),
                    append = FALSE, sep = ',', na = '',
                    row.names = FALSE, col.names = TRUE, fileEncoding = 'UTF-8')
